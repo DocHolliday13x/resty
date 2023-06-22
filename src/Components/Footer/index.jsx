145e8b044f64fee5a12f05c7c911f91ad1fa5344
Ryan Eastman <rking1323@gmail.com>
2023-06-21 19:18:15 -0700

    WE HAVE A WORKING REACT APP

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+function Footer() {
+  return <footer>&copy; Uncle Buzz's Basement 2023</footer>;
+}
+
+export default Footer;
