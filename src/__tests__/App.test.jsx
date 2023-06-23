63c4696e1683e6c306219b817e647ab60100842e
Ryan Eastman <rking1323@gmail.com>
2023-06-22 17:58:45 -0700

    initial documentation and planning

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
-import Form from './Components/Form/index.jsx';
+import App from '../App';
+import Form from '../Components/Form/index.jsx';
 
 describe('results', () => {
   it('should render results', () => {
