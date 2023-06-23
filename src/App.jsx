d3ab99be0b9e1da73efd882ed01e0f5e6c3dbb84
Ryan Eastman <rking1323@gmail.com>
2023-06-22 18:30:37 -0700

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 import './App.scss';
 import Header from './Components/Header';
@@ -6,73 +7,64 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
+
 function App() {
   const [data, setData] = React.useState(null);
   const [requestParams, setRequestParams] = React.useState({});
   const [loading, setLoading] = React.useState(false);
 
+  useEffect(() => {
+    console.log('Event Occured!');
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      if (requestParams.method === 'GET') {
+        let response = await fetch(requestParams.url);
+        setData(response.data.results)
+      }
+      if (requestParams.method === 'POST') {
+        let response = await axios.post(requestParams.url, requestParams.json);
+        setData(response.data.results);
+      }
+      if (requestParams.method === 'PUT') {
+        let response = await axios.put(requestParams.url, requestParams.json);
+        setData(response.data.results);
+      }
+      if (requestParams.method === 'DELETE') {
+        let response = await axios.delete(requestParams.url);
+        setData(response.data.results);
+      }
+    }
+    if (requestParams.method && requestParams.url) {
+      fetchData();
+    }
+  }, [requestParams]);
+
   const callApi = (requestParams) => {
     setLoading(true);
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    setData(data);
     setRequestParams(requestParams);
     setLoading(false);
   }
+  
 
   return (
     <React.Fragment>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
+      <div data-testid="app-method" className="divvy">Request Method: {requestParams.method}</div>
+      <div data-testid="app-url" className="divvy">URL: {requestParams.url}</div>
+      {
+        requestParams.json
+          ? <div data-testid="app-json" className="divvy">JSON: {requestParams.json}</div>
+          : <div></div>
+      }
       <Form handleApiCall={callApi} />
       <Results data={data} loading={loading} />
       <Footer />
     </React.Fragment>
   );
   
-}
+};
 
-// class App extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: null,
-//       requestParams: {},
-//     };
-//   }
-
-//   callApi = (requestParams) => {
-//     // mock output
-//     const data = {
-//       count: 2,
-//       results: [
-//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-//       ],
-//     };
-//     this.setState({data, requestParams});
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Header />
-//         <div>Request Method: {this.state.requestParams.method}</div>
-//         <div>URL: {this.state.requestParams.url}</div>
-//         <Form handleApiCall={this.callApi} />
-//         <Results data={this.state.data} />
-//         <Footer />
-//       </React.Fragment>
-//     );
-//   }
-// }
 
 export default App;
