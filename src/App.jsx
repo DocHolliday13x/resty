769400a4798739f528cc09813016d017aa29daf6
Ryan Eastman <rking1323@gmail.com>
2023-06-22 19:22:48 -0700

    starting debugging app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,30 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
-
 function App() {
   const [data, setData] = React.useState(null);
   const [requestParams, setRequestParams] = React.useState({});
   const [loading, setLoading] = React.useState(false);
 
   useEffect(() => {
-    console.log('Event Occured!');
-  });
-
-  useEffect(() => {
     async function fetchData() {
       if (requestParams.method === 'GET') {
         let response = await fetch(requestParams.url);
-        setData(response.data.results)
-      }
-      if (requestParams.method === 'POST') {
-        let response = await axios.post(requestParams.url, requestParams.json);
-        setData(response.data.results);
-      }
-      if (requestParams.method === 'PUT') {
-        let response = await axios.put(requestParams.url, requestParams.json);
-        setData(response.data.results);
-      }
-      if (requestParams.method === 'DELETE') {
-        let response = await axios.delete(requestParams.url);
+        let jsonData = await response.json();
+        setData(jsonData.results);
+      } else {
+        let response;
+        if (requestParams.method === 'POST') {
+          response = await axios.post(requestParams.url, requestParams.json);
+        } else if (requestParams.method === 'PUT') {
+          response = await axios.put(requestParams.url, requestParams.json);
+        } else if (requestParams.method === 'DELETE') {
+          response = await axios.delete(requestParams.url);
+        }
         setData(response.data.results);
       }
     }
+
     if (requestParams.method && requestParams.url) {
       fetchData();
     }
@@ -45,26 +40,30 @@ function App() {
     setLoading(true);
     setRequestParams(requestParams);
     setLoading(false);
-  }
-  
+  };
 
   return (
     <React.Fragment>
       <Header />
-      <div data-testid="app-method" className="divvy">Request Method: {requestParams.method}</div>
-      <div data-testid="app-url" className="divvy">URL: {requestParams.url}</div>
-      {
-        requestParams.json
-          ? <div data-testid="app-json" className="divvy">JSON: {requestParams.json}</div>
-          : <div></div>
-      }
+      <div data-testid="app-method" className="divvy">
+        Request Method: {requestParams.method}
+      </div>
+      <div data-testid="app-url" className="divvy">
+        URL: {requestParams.url}
+      </div>
+      {requestParams.json ? (
+        <div data-testid="app-json" className="divvy">
+          JSON: {requestParams.json}
+        </div>
+      ) : (
+        <div></div>
+      )}
       <Form handleApiCall={callApi} />
       <Results data={data} loading={loading} />
       <Footer />
     </React.Fragment>
   );
-  
-};
-
+}
 
 export default App;
+
